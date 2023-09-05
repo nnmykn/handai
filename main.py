3c30b168fbfdd57657ceac6081f862821ba9bdd5
ninomiya <hi@kan.run>
2023-09-05 21:15:33 +0900

    record

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
+import wave
+
+import pyaudio
 from pynput import keyboard
+
 
 class AKeyListener:
     def __init__(self):
         self.key_pressed = False
+        self.recording = False
+        self.frames = []
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+    def start_recording(self):
+        print("録音を開始します")
+        self.recording = True
+        self.frames = []
+        while self.recording:
+            data = self.stream.read(1024)
+            self.frames.append(data)
+
+    def stop_recording(self):
+        print("録音を停止します")
+        self.recording = False
+        with wave.open('output.wav', 'wb') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
+            wf.setframerate(44100)
+            wf.writeframes(b''.join(self.frames))
+        self.frames = []
 
     def on_press(self, key):
-        if key == keyboard.Key.a:
-            self.key_pressed = True
-            print("Aキーを押しています")
+        try:
+            if key.char == 'a' and not self.key_pressed:
+                self.key_pressed = True
+                print("Aキーを押しています")
+                self.start_recording()
+        except AttributeError:
+            pass
 
     def on_release(self, key):
-        if key == keyboard.Key.a:
-            self.key_pressed = False
-            print("Aキーを離しました")
-            # Aキーを離したときの処理を以下に追加
-            self.stop_listener()
+        try:
+            if key.char == 'a':
+                self.key_pressed = False
+                print("Aキーを離しました")
+                self.stop_recording()
+        except AttributeError:
+            pass
 
     def stop_listener(self):
         self.listener.stop()
